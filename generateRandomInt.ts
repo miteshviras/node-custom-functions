e3da365bc4c42064ce2563c5ed94db2a9ff3394e
Scraper329 <87187589+miteshviras@users.noreply.github.com>
2023-11-24 13:06:59 +0530

    Create generateRandomInt.ts
    generate random number like php rand(start,end)

---
+++ generateRandomInt.ts
@@ -0,0 +1,11 @@
+// get random digits
+export function generateRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Export the specific function you want to import in other files
+module.exports = {
+  generateRandomInt
+};
