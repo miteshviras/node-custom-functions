6fcf2cf549f8463be465003c45e4fb77cb93b069
Scraper329 <87187589+miteshviras@users.noreply.github.com>
2023-12-11 11:00:36 +0530

    Update generateRandomInt.ts

--- generateRandomInt.ts
+++ generateRandomInt.ts
@@ -1,5 +1,5 @@
 // get random digits
-export function generateRandomInt(min, max) {
+export function generateRandomInt(min : number, max : number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
